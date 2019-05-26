01c6f410381aadaccdd9b50f4c6b054f33404b9a
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-05-26 15:31:54 +0100

    not case sensitive password with map method

--- main.js
+++ main.js
@@ -12,14 +12,25 @@ const messages = ["works", "working"]
 
 //----------forEach()--------
 
-passwords.forEach((password, index) => {
-    passwords[index] = password.toLocaleLowerCase();
-})
+// passwords.forEach((password, index) => {
+//     passwords[index] = password.toLocaleLowerCase();
+// })
 
-const showMessage = (e) => {
-    passwords.forEach((password, index) => {
-        const input = e.target.value
-        if (password === input) {
+// const showMessage = (e) => {
+//     passwords.forEach((password, index) => {
+//         const input = e.target.value
+//         if (password === input) {
+//             document.querySelector('div').textContent = messages[index];
+//         }
+//     })
+// }
+
+//-----------map()---------------
+
+const lowerCasePasswords = passwords.map(password => password.toLocaleLowerCase()); // if written fat arrow in one line then it works like tith return. 
+const showMessage = (event) => {
+    lowerCasePasswords.forEach((item, index) => {
+        if (item === event.target.value) {
             document.querySelector('div').textContent = messages[index];
         }
     })
